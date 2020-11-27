8b95ef746a0eda799486cb9f26b225e08e93cc03
huyenlothi <69282948+huyenlothi@users.noreply.github.com>
2020-11-27 08:57:34 +0700

    add file

--- src/main/java/com/quanli/controller/ProvinceController.java
+++ src/main/java/com/quanli/controller/ProvinceController.java
@@ -1,10 +1,10 @@
 package com.quanli.controller;
 
+import com.quanli.exception.NotFoundException;
 import com.quanli.model.Province;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 import service.province.IProvinceService;
 
@@ -21,6 +21,36 @@ public class ProvinceController {
         modelAndView.addObject("province", provinces);
         return modelAndView;
     }
-
+    @GetMapping("/create")
+    public ModelAndView createForm(){
+        ModelAndView modelAndView= new ModelAndView("province/createProvince");
+        modelAndView.addObject("province",new Province());
+        return modelAndView;
+    }
+    @PostMapping("/create")
+    public ModelAndView create(@ModelAttribute Province province){
+        ModelAndView modelAndView= new ModelAndView("province/createProvince");
+        provinceService.save(province);
+        modelAndView.addObject("province",province);
+        return modelAndView;
+    }
+    @GetMapping("/edit/{id}")
+    public ModelAndView editForm(@PathVariable Long id){
+        ModelAndView modelAndView= new ModelAndView("province/edit");
+        try {
+            Province province= provinceService.findById(id);
+            modelAndView.addObject("province",province);
+        } catch (NotFoundException e) {
+            e.printStackTrace();
+        }
+        return modelAndView;
+    }
+    @PostMapping("/edit/{id}")
+    public ModelAndView edit(@ModelAttribute Province province){
+        ModelAndView modelAndView= new ModelAndView("province/edit");
+        provinceService.save(province);
+        modelAndView.addObject("province",province);
+        return modelAndView;
+    }
 
 }
